f04178ee8dc7bfce4ee8fd119eeb954cc30e0a78
Clare <krysteenclarebelen@gmail.com>
2024-05-28 14:25:31 +0800

    Added Image Directory

--- src/main/java/com/groupten/bmsproject/BmsprojectApplication.java
+++ src/main/java/com/groupten/bmsproject/BmsprojectApplication.java
@@ -26,7 +26,7 @@ public class BmsprojectApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/DisplayOrder.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Dashboard.fxml"));
 
         // Set the Spring context for the FXMLLoader
         loader.setControllerFactory(applicationContext::getBean);
