cc7e076e345c95841bbef9f7eaa962744c7b8060
KrazyHackerist <91444392+KrazyHackerist@users.noreply.github.com>
2024-06-17 13:00:42 +0800

    Order Registration: Product Order is now a Choice box Added Warning dialog if low stock Not showing the zero quantity product on Choice box

--- src/main/java/com/groupten/bmsproject/FXMLControllers/OrderingController.java
+++ src/main/java/com/groupten/bmsproject/FXMLControllers/OrderingController.java
@@ -3,22 +3,29 @@ package com.groupten.bmsproject.FXMLControllers;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-import javafx.util.Callback;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.util.Callback;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
 import com.groupten.bmsproject.BmsprojectApplication;
+import com.groupten.bmsproject.Inventory.InventoryEntity;
 import com.groupten.bmsproject.Order.OrderEntity.DeliveryStatus;
 import com.groupten.bmsproject.Order.OrderEntity.PaymentStatus;
 import com.groupten.bmsproject.Order.OrderService;
+import com.groupten.bmsproject.Product.ProductService;
+import com.groupten.bmsproject.Product.ProductEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
@@ -29,29 +36,34 @@ public class OrderingController {
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private ProductService productService;
+
     @FXML
     private TextField CustomerNameTextField;
-    
+
     @FXML
     private TextField AddressTextField;
-    
+
     @FXML
     private DatePicker DateOrder;
-    
+
     @FXML
-    private TextField ProductOrderTextField;
-    
+    private ChoiceBox<String> ProductOrderChoiceBox;
+
     @FXML
     private TextField QuantityOrderTextField;
-    
+
     @FXML
     private ComboBox<PaymentStatus> PaymentStatusComboBox;
-    
+
     @FXML
     private ComboBox<DeliveryStatus> DeliveryStatusComboBox;
 
     @FXML
-    private Button saveOrderButton;
+    private Button SaveOrderButton;
+
+    private static final int LOW_STOCK_THRESHOLD = 5;
 
     @FXML
     private void initialize() {
@@ -60,6 +72,16 @@ public class OrderingController {
 
         // Disable past dates in the DatePicker
         DateOrder.setDayCellFactory(getDateCellFactory());
+
+        // Populate ProductOrderChoiceBox with available products
+        populateProductOrderChoiceBox();
+    }
+
+    private void populateProductOrderChoiceBox() {
+        List<ProductEntity> availableProducts = productService.getAvailableProducts();
+        for (ProductEntity product : availableProducts) {
+            ProductOrderChoiceBox.getItems().add(product.getproductName());
+        }
     }
 
     private Callback<DatePicker, DateCell> getDateCellFactory() {
@@ -87,11 +109,28 @@ public class OrderingController {
         String customerName = CustomerNameTextField.getText();
         String address = AddressTextField.getText();
         LocalDateTime dateOrder = DateOrder.getValue().atStartOfDay(); // Convert DatePicker value to LocalDateTime
-        String productOrder = ProductOrderTextField.getText();
+        String productOrder = ProductOrderChoiceBox.getValue();
         Integer quantityOrder = Integer.parseInt(QuantityOrderTextField.getText());
         PaymentStatus paymentStatus = PaymentStatusComboBox.getValue();
         DeliveryStatus deliveryStatus = DeliveryStatusComboBox.getValue();
 
+        // Check for low stock products
+        List<ProductEntity> lowStockProducts = productService.getLowStockProducts(LOW_STOCK_THRESHOLD);
+        if (!lowStockProducts.isEmpty()) {
+            StringBuilder warningMessage = new StringBuilder("Warning: Low stock on the following products:\n");
+            for (ProductEntity product : lowStockProducts) {
+                warningMessage.append(product.getproductName()).append(" (Quantity: ").append(product.productQuantity()).append(")\n");
+            }
+            
+            // Show a warning dialog
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Low Stock Warning");
+            alert.setHeaderText("There are low stock products.");
+            alert.setContentText(warningMessage.toString());
+            alert.showAndWait();
+        }
+
+        // Proceed to save the order
         String result = orderService.addNewOrder(customerName, address, dateOrder, productOrder, quantityOrder, paymentStatus, deliveryStatus);
         System.out.println(result);
     }
--- src/main/java/com/groupten/bmsproject/Product/ProductRepository.java
+++ src/main/java/com/groupten/bmsproject/Product/ProductRepository.java
@@ -1,8 +1,11 @@
 package com.groupten.bmsproject.Product;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Integer> {
-    
+    List<ProductEntity> findByQuantityGreaterThan(Integer quantity);
+    List<ProductEntity> findByQuantityLessThanEqual(Integer quantity);
 }
