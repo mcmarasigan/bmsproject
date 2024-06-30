40c6ac3d8a2719a9f383b8f481158df2ea87f026
KrazyHackerist <91444392+KrazyHackerist@users.noreply.github.com>
2024-07-01 01:47:12 +0800

    Update: Save report file with timestamp

--- src/main/java/com/groupten/bmsproject/JasperReport/ReportService.java
+++ src/main/java/com/groupten/bmsproject/JasperReport/ReportService.java
@@ -2,18 +2,17 @@ package com.groupten.bmsproject.JasperReport;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import java.util.jar.JarException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
 
-import com.groupten.bmsproject.Admin.Adminentity;
-import com.groupten.bmsproject.Admin.Adminrepository;
 import com.groupten.bmsproject.Ingredient.IngredientEntity;
 import com.groupten.bmsproject.Ingredient.IngredientRepository;
 import com.groupten.bmsproject.Sales.SalesEntity;
@@ -26,46 +25,47 @@ import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
-import net.sf.jasperreports.engine.design.JasperDesign;
 
 @Service
 public class ReportService {
 
-   @Autowired
-   private IngredientRepository ingredientRepository;
-   @Autowired
-   private SalesRepository salesRepository;
+    @Autowired
+    private IngredientRepository ingredientRepository;
+    @Autowired
+    private SalesRepository salesRepository;
 
+    public String exportReport(String reportFormat) throws FileNotFoundException, JRException {
+        String path = "C:\\Users\\Maria\\3rd Year Projects\\softeng\\bmsproject\\src\\main\\java\\com\\groupten\\bmsproject\\Report Files";
 
-   public String exportReport (String reportFormat) throws FileNotFoundException, JRException{
-    String path ="C:\\Users\\Maria\\3rd Year Projects\\softeng\\bmsproject\\src\\main\\java\\com\\groupten\\bmsproject\\Report Files";
+        List<IngredientEntity> ingredients = ingredientRepository.findAll();
 
-    List<IngredientEntity> ingredients = ingredientRepository.findAll();
+        // Load file and compile
+        File file = ResourceUtils.getFile("classpath:ReportTemplate/Ingredient_Report.jrxml");
+        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(ingredients);
 
-    //Load file and compile
-    File file= ResourceUtils.getFile(("classpath:ReportTemplate/Ingredient_Report.jrxml"));
-    JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
-    JRBeanCollectionDataSource dataSource= new JRBeanCollectionDataSource(ingredients);
+        // Generate a UUID
+        String uuid = UUID.randomUUID().toString();
+        // Take the first 5 characters as the report ID
+        String reportId = uuid.substring(0, 5);
 
-       // Generate a UUID
-       String uuid = UUID.randomUUID().toString();
+        Map<String, Object> map = new HashMap<>();
+        map.put("REPORT_ID", reportId);
+        map.put("createdBy", "Developer");
 
-       // Take the first 5 characters as the report ID
-       String reportId = uuid.substring(0, 5);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, map, dataSource);
 
-    Map<String,Object> map=new HashMap<>();
-    map.put("REPORT_ID", reportId);
-    map.put("createdBy","Developer");
+        if (reportFormat.equalsIgnoreCase("pdf")) {
+            // Format the current date and time
+            String timestamp = new SimpleDateFormat("dd-MM-yyyy_hh-mm a").format(new Date());
+            String fileName = String.format("%s\\IngredientReport_%s.pdf", path, timestamp);
+            JasperExportManager.exportReportToPdfFile(jasperPrint, fileName);
+            return "report generated in path: " + fileName;
+        }
+        return "Unsupported report format: " + reportFormat;
+    }
 
-    JasperPrint jasperPrint= JasperFillManager.fillReport(jasperReport, map, dataSource);
-
-    if(reportFormat.equalsIgnoreCase("pdf")){
-JasperExportManager.exportReportToPdfFile(jasperPrint, path+"\\ingredient_report.pdf");
-}
-    return "report generated in path:"+path;
-   }
-
-   public String exportSalesReport(String reportFormat) throws FileNotFoundException, JRException {
+    public String exportSalesReport(String reportFormat) throws FileNotFoundException, JRException {
         String path = "C:\\Users\\Maria\\3rd Year Projects\\softeng\\bmsproject\\src\\main\\java\\com\\groupten\\bmsproject\\Report Files";
 
         List<SalesEntity> sales = salesRepository.findAll();
@@ -75,21 +75,24 @@ JasperExportManager.exportReportToPdfFile(jasperPrint, path+"\\ingredient_report
         JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
         JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(sales);
 
-// Generate a UUID
-String uuid = UUID.randomUUID().toString();
+        // Generate a UUID
+        String uuid = UUID.randomUUID().toString();
+        // Take the first 5 characters as the report ID
+        String reportId = uuid.substring(0, 5);
 
-// Take the first 5 characters as the report ID
-String reportId = uuid.substring(0, 5);
+        Map<String, Object> map = new HashMap<>();
+        map.put("REPORT_ID", reportId);
+        map.put("createdBy", "Developer");
 
-    Map<String,Object> map=new HashMap<>();
-    map.put("REPORT_ID", reportId);
-    map.put("createdBy","Developer");
         JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, map, dataSource);
 
         if (reportFormat.equalsIgnoreCase("pdf")) {
-            JasperExportManager.exportReportToPdfFile(jasperPrint, path + "\\sales_report.pdf");
+            // Format the current date and time
+            String timestamp = new SimpleDateFormat("dd-MM-yyyy_hh-mm a").format(new Date());
+            String fileName = String.format("%s\\SalesReport_%s.pdf", path, timestamp);
+            JasperExportManager.exportReportToPdfFile(jasperPrint, fileName);
+            return "report generated in path: " + fileName;
         }
-        return "report generated in path:" + path;
+        return "Unsupported report format: " + reportFormat;
     }
 }
-
