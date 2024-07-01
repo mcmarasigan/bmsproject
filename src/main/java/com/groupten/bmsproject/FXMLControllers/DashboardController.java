6f89d2a6e7be256d06d8c2244b4951afb642e58f
KrazyHackerist <91444392+KrazyHackerist@users.noreply.github.com>
2024-07-01 17:00:31 +0800

    Update: Added Help Module User manual

--- src/main/java/com/groupten/bmsproject/FXMLControllers/DashboardController.java
+++ src/main/java/com/groupten/bmsproject/FXMLControllers/DashboardController.java
@@ -148,7 +148,18 @@ public class DashboardController {
         stage.setScene(scene);
         stage.show();
     }
+    @FXML
+    private void proceedHelpModule() throws IOException {
+        ConfigurableApplicationContext context = BmsprojectApplication.getApplicationContext(); // Get the application context
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/HelpModule.fxml"));
+        loader.setControllerFactory(context::getBean);
 
+        Parent root = loader.load();
+        Stage stage = BmsprojectApplication.getPrimaryStage();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
     @FXML
     private void Logout() throws IOException {
         // Show confirmation dialog
