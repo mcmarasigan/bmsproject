f2a14198784f64878388ae8d81b3a4fb8c10dadc
KrazyHackerist <91444392+KrazyHackerist@users.noreply.github.com>
2024-07-01 14:36:18 +0800

    Update: add back to dashboard in account management

--- src/main/java/com/groupten/bmsproject/FXMLControllers/AccountManagementController.java
+++ src/main/java/com/groupten/bmsproject/FXMLControllers/AccountManagementController.java
@@ -88,8 +88,16 @@ public class AccountManagementController {
     }
 
     @FXML
-    private void backToDashboard() {
-        
+    private void backToDashboard() throws IOException {
+        ConfigurableApplicationContext context = BmsprojectApplication.getApplicationContext(); // Get the application context
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Dashboard.fxml"));
+        loader.setControllerFactory(context::getBean);
+
+        Parent root = loader.load();
+        Stage stage = BmsprojectApplication.getPrimaryStage();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
